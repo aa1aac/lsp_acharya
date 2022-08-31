65f7fcc526654b9128346036f74ccad771a820aa
aa1aac <35446762+aa1aac@users.noreply.github.com>
2022-08-30 23:42:05 -0400

    Commit for assignment #1.

---
+++ lsp_acharya/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.assignment1;
+
+/** 
+ * Aayush Acharya
+ * @author Aayush
+ * **/
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Large Scale Programming!!");
+
+	}
+
+}
